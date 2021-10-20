967af7ca6311035a123426abd5056507f8654557
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-19 17:53:28 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key 
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # This module is now only being tested with Terraform 0.15.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.15.x code.
+  required_version = ">= 0.15.3"
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+   region  = var.aws_region
+   access_key = var.access_key
+   secret_key = var.secret_key
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE S3 BUCKET
+# ------------------------------------------------------------------------------
+
+resource "aws_s3_bucket" "chatsfeed-terraform-remote-state-s3" {
+  # TODO: change this to your own name! S3 bucket names must be *globally* unique.
+  bucket = "chatsfeed-terraform-remote-state-s3"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE DYNAMODB TABLE
+# ------------------------------------------------------------------------------
+
+resource "aws_dynamodb_table" "chatsfeed-terraform-remote-state-s3-locks" {
+  name         = "chatsfeed-terraform-remote-state-s3-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
